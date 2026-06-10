d4f78ac307c6fb0ce1386f2f612fa4dffb2f9283
abdullahzafar08 <857895.r@gmail.com>
2026-06-10 11:00:00 +0500

    work: implement and structure files in legacy (part 47)

---
+++ legacy/sample/components/DashboardLayout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { DashboardSidebar } from "./DashboardSidebar";
+
+export const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-background">
+      <DashboardSidebar />
+      <main className="flex-1 flex flex-col min-w-0">
+        {children}
+      </main>
+    </div>
+  );
+};
